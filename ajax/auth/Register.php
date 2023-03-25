83268081e0cf847220fc9ab22e5ca7be8b16c966
Mauricio Gonzalez Morales <mauriciogonzalez325@hotmail.com>
2023-03-24 20:50:17 -0600

    Se obtienen valores POST para realizar el registro del usuario

--- ajax/auth/Register.php
+++ ajax/auth/Register.php
@@ -1,3 +1,9 @@
 <?php
 require_once "../../controllers/UserController.php";
 require_once "../../controllers/PHPMailerController.php";
+$html = '';
+//Datos
+$name = $_POST['name'];
+$correo = $_POST['email'];
+$pass = $_POST['password'];
+$subject = "Confirmación de correo electrónico";
