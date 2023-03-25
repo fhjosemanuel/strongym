7a255215966543a27f5262a49b5dbbb155d60620
Fernando Cortés <fernando.cortes971@gmail.com>
2023-03-25 10:19:17 -0600

    Se añadió funcionalidad necesarios para actualizar estado del usuario en ConfirmRegister.php

--- ajax/auth/ConfirmRegister.php
+++ ajax/auth/ConfirmRegister.php
@@ -1,2 +1,14 @@
 <?php
 require_once "../../controllers/UserController.php";
+
+function obtenerUsuario($email) {
+  $userObject = new UserController();
+  $datosUsuario = $userObject->email($email);
+  return $datosUsuario;
+}
+
+function actualizarEstatus($user) {
+  $userObject = new UserController();
+  $actualizarEstado = $userObject->update("validated = true", "id = $user");
+  return $actualizarEstado;
+}
