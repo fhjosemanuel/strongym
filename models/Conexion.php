cb1655f8eb07f88f8d9a49466b8d6202f6a481af
JOSE MANUEL FLORES HERNANDEZ <fhjosemanuel@gmail.com>
2023-03-24 20:27:53 -0600

    Se agrega métodos para abrir y cerrar la Base de DAtos

--- models/Conexion.php
+++ models/Conexion.php
@@ -10,6 +10,13 @@ abstract class Conexion{
     private $conn;
     protected $query;
     protected $rows = array();
+    private function db_open(){
+        $this->conn = new mysqli(self::$db_host,self::$db_user,self::$db_pass,self::$db_name);
+        $this->conn->set_charset(self::$db_charset);
+    }
+    private function db_close(){
+        $this->conn->close();
+    }
     
 }
 ?>
