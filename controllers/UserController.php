af7eaeec10c6246e84d2a335f8dcac2170454aa7
Mauricio Gonzalez Morales <mauriciogonzalez325@hotmail.com>
2023-03-24 20:46:57 -0600

    Se agregan funciones para realizar operaciones CRUD

--- controllers/UserController.php
+++ controllers/UserController.php
@@ -20,4 +20,22 @@ class UserController{
     {
         return $this->ejecuta->user( $email, $password );
     }
+
+    public function create( $values )
+    {
+        return $this->ejecuta->create( $values );
+    }
+    public function read()
+    {
+        return $this->ejecuta->read();
+    }
+    public function update($fillable, $conditions)
+    {
+        return $this->ejecuta->update( $fillable, $conditions );
+    }
+    public function delete($id, $profile)
+    {
+        return $this->ejecuta->delete( $id, $profile );
+    }
+    
 }
