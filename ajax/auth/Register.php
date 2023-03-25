532f03515b31112b8f5f826a0e968b72ef661f86
Mauricio Gonzalez Morales <mauriciogonzalez325@hotmail.com>
2023-03-24 20:52:34 -0600

    Se añade el proceso para el registro del usuario y el envio del correo

--- ajax/auth/Register.php
+++ ajax/auth/Register.php
@@ -7,3 +7,34 @@ $name = $_POST['name'];
 $correo = $_POST['email'];
 $pass = $_POST['password'];
 $subject = "Confirmación de correo electrónico";
+
+$myAccount = new UserController();
+$account = $myAccount->email($correo);
+if( empty( $account ) ) {
+    //Generar código de verificación
+    $generarCode = new PHPMailerController();
+    $verification_code = $generarCode->generateVerificationCode($correo);
+    
+    // Crear cuenta
+    $info = [
+        $name,
+        $correo,
+        $pass,
+        $verification_code
+    ];
+    $myAccount = new UserController();
+    $account = $myAccount->create($info);
+
+    $url_confirmation = "http://strongym.test/views/auth/confirm.php?email=" . urlencode($correo) . "&token=" . urlencode($verification_code);
+
+    //Enviar correo
+    $email = new PHPMailerController();
+    $send = $email->sendEmail( $name, $correo, $subject, $url_confirmation, 'registro' );    
+} else {
+    $html .= '
+    <div class="alert alert-info" role="alert">
+        El correo electrónico ya se encuentra registrado. <a href="/views/auth/login.php">Iniciar sesión</a>
+    </div>
+    ';
+}
+echo $html;
